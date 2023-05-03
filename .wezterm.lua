438c7ecc3db1559f325e138c1bc859656e1df88d
Chris Jordan <chrisjordan82@gmail.com>
2023-05-03 23:27:51 +0100

    automate collation with gather.sh; finish shift to tokyonight-storm

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,7 +10,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = "Catppuccin Macchiato"
+config.color_scheme = "tokyonight-storm"
 
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 12
---
+++ gather.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# terminal
+cp ~/.wezterm.lua .
+
+# multiplexer
+cp ~/.tmux.conf .
+
+# shells
+cp ~/.zshrc .
+cp ~/.p10k.zsh .
+
+# neovim
+cp -r ~/.config/nvim/ ./.config
