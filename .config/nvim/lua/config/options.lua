1d39faf092c81adcc12c060828d78c4c6b54d7cf
Chris Jordan <chrisjordan82@gmail.com>
2023-07-10 14:05:22 +0100

    set tabstop to 4

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local opt = vim.opt
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+
 vim.g.python3_host_prog = "/usr/bin/python3"
 vim.cmd("aunmenu PopUp.How-to\\ disable\\ mouse")
 vim.cmd("aunmenu PopUp.-1-")
